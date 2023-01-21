4d1b9eaff5ae23a0a067bf262db582843dd8cbe9
mateus <mateuspastor@outlook.com>
2023-01-21 13:43:59 -0300

    Otimização de imagens

--- src/js/gmaps.js
+++ src/js/gmaps.js
@@ -8,12 +8,13 @@ function initMap() {
         zoomControl: true
     });
 
+    const markerIconSrc = "https://emagrecentrocarlosbarbosa.com.br/public/images/gmaps/icon/marker.webp";
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
         position: position,
         icon: {
             size: new google.maps.Size(220, 220),
-            scaledSize: new google.maps.Size(35, 50),
+            scaledSize: new google.maps.Size(35, 45),
             origin: new google.maps.Point(0, 0),
             url: markerIconSrc,
             anchor: new google.maps.Point(15, 50)
@@ -55,6 +56,7 @@ function openWindowInfo(infowindow, map, marker) {
 
     content.appendChild(contentRigth);
 
+    const inforWindowImgSrc = "https://emagrecentrocarlosbarbosa.com.br/public/images/gmaps/info/emagrecentro-info.webp";
     const contentLeft = document.createElement("figure");
     contentLeft.classList.add('map-info-img');
     const img = document.createElement("img");
